aa7e350fcb3726420d82353f70cfbbfe6d1e9ad9
thiaggom <thiaggom@gmail.com>
2018-06-01 10:41:26 -0300

    Adding ArgumentCaptor to IndexControllerTest

--- recipe-project/src/test/java/tmelo/recipeproject/controllers/IndexControllerTest.java
+++ recipe-project/src/test/java/tmelo/recipeproject/controllers/IndexControllerTest.java
@@ -5,13 +5,19 @@ import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import tmelo.recipeproject.domain.Recipe;
 import tmelo.recipeproject.services.RecipesService;
 
 public class IndexControllerTest {
@@ -33,6 +39,18 @@ public class IndexControllerTest {
 	
 	@Test
 	public void getIndexPage() throws Exception {
+	
+		Set<Recipe> recipes = new HashSet<>();
+		Recipe rec1 = new Recipe();
+		rec1.setDescription("recipe 1");
+		Recipe rec2 = new Recipe();
+		rec2.setDescription("recipe 2");
+		recipes.add(rec1);
+		recipes.add(rec2);
+
+		when(recipesService.getAllRecipes()).thenReturn(recipes);
+		
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 		
 		String returnedPage = indexController.getIndexPage(model);
 		assertEquals("index", returnedPage);
@@ -41,6 +59,9 @@ public class IndexControllerTest {
 		verify(recipesService, times(1)).getAllRecipes();
 		
 		// verifing if the recipes attribute was set in the page model
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		
+		// verifing if the object returned in the model was the Set with 2 positions...
+		assertEquals(2, argumentCaptor.getValue().size());
 	}
 }
